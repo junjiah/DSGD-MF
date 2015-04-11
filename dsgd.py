b06287b2c3efac3107b539e477e1fc54fb057d50
EDFward <edfward@gmail.com>
2015-04-10 21:24:06 -0400

    used ndarray innstead of matrix; wrote parameters; recorded time usage;

--- dsgd.py
+++ dsgd.py
@@ -1,12 +1,11 @@
 import os
-from scipy import sparse
 import sys
 import numpy as np
-from numpy.random import rand
-from numpy import matrix
 
+from numpy.random import rand
 from operator import itemgetter
-from pyspark import SparkContext, SparkConf
+from pyspark import SparkContext
+import time
 
 
 def get_rating(file_content):
@@ -47,14 +46,16 @@ if __name__ == '__main__':
         ratings = sc.textFile(inputV_filepath).map(
             lambda line: map(int, line.split(','))).cache()
 
+    start_time = time.time()
+
     # row / col sizes are max user / movie index
-    # TODO: DEBUG
+    # TODO:
     row_number = ratings.max(key=itemgetter(0))[0] + 1
     col_number = ratings.max(key=itemgetter(1))[1] + 1
 
     # build W and H
-    user_factor = matrix(rand(row_number, num_factors))
-    movie_factor = matrix(rand(col_number, num_factors))
+    user_factor = rand(row_number, num_factors)
+    movie_factor = rand(col_number, num_factors)
 
     # slice ratings matrix to column groups
     # TODO: what if not exact division
@@ -79,7 +80,6 @@ if __name__ == '__main__':
         :return:
         """
         column_group, rating_entries = group
-        i = 0
 
         for user, movie, rating in rating_entries:
             # transform real indexes to partitioned factor matrix indexes
@@ -87,23 +87,15 @@ if __name__ == '__main__':
             user_index = (user - 0) % block_row_size
             movie_index = (movie - 0) % block_column_size
 
-            pred_rating = u_f_p[user_index, :] * \
-                          m_f_p[movie_index, :].T
-
-            if i % 500 == 0:
-                print 'pred: ' + str(pred_rating)
-
-            i += 1
+            pred_rating = np.dot(u_f_p[user_index, :], m_f_p[movie_index, :])
 
             # TODO: add regularization terms
-            # u_f_p[user_index, :] -= beta_value * (
-            #     -2 * (rating - pred_rating) * m_f_p[movie_index, :])
             u_gradient = -2 * (rating - pred_rating) * m_f_p[movie_index, :] + \
-                2 * lambda_value / col_number * u_f_p[user_index, :]
+                         2 * lambda_value / col_number * u_f_p[user_index, :]
             u_f_p[user_index, :] -= beta_value * u_gradient
 
             m_gradient = -2 * (rating - pred_rating) * u_f_p[user_index, :] + \
-                2 * lambda_value / row_number * m_f_p[movie_index, :]
+                         2 * lambda_value / row_number * m_f_p[movie_index, :]
             m_f_p[movie_index, :] -= beta_value * m_gradient
 
         return column_group, (u_f_p, m_f_p)
@@ -118,7 +110,7 @@ if __name__ == '__main__':
             .groupBy(lambda r: r[1] / block_column_size, num_workers) \
             .map(lambda group: update(group,
                                       user_factors[strata[group[0]]],
-                                      movie_factors[group[0]])) \
+                                      movie_factors[group[0]]), True) \
             .collect()
 
         # aggregate the updates
@@ -134,14 +126,26 @@ if __name__ == '__main__':
         # shift the strata
         strata.append(strata.pop(0))
 
-    rating_matrix = sparse.dok_matrix((col_number, row_number), dtype=float)
+    # do simple evaluation
+    end_time = time.time()
+    pred_ratings = np.dot(user_factor, movie_factor.T)
+    rmse, rating_count = 0.0, 0
     for u, m, r in ratings.collect():
-        rating_matrix[(m, u)] = r
-
-    pred_ratings = movie_factor * user_factor.T
-
-    diff = rating_matrix.tocsr() - pred_ratings
-    rmse = np.sqrt(np.sum(np.power(diff, 2)) / col_number * row_number)
-    print rmse
+        rmse += (r - pred_ratings[u, m]) ** 2
+        rating_count += 1
 
     sc.stop()
+
+    rmse = np.sqrt(rmse / rating_count)
+    print
+    print 'RMSE: ' + str(rmse)
+    print 'time usage: %s seconds' % (end_time - start_time)
+
+    # write parameters
+    with open(outputW_filepath, 'wb') as f:
+        for row in user_factor:
+            f.write(','.join(map(str, row)) + '\n')
+
+    with open(outputH_filepath, 'wb') as f:
+        for row in movie_factor:
+            f.write(','.join(map(str, row)) + '\n')
