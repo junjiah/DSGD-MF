afb878fc28f968a9e43b1fac714f35258e15c090
EDFward <edfward@gmail.com>
2015-04-11 00:49:50 -0400

    refactored code for cleaner look

--- dsgd.py
+++ dsgd.py
@@ -36,7 +36,7 @@ if __name__ == '__main__':
     beta_value, lambda_value = map(float, sys.argv[4:6])
     inputV_filepath, outputW_filepath, outputH_filepath = sys.argv[6:]
 
-    sc = SparkContext('local', 'Distributed Stochastic Gradient Descent')
+    sc = SparkContext('local[8]', 'Distributed Stochastic Gradient Descent')
 
     if os.path.isdir(inputV_filepath):
         rating_files = sc.wholeTextFiles(inputV_filepath)
@@ -46,103 +46,106 @@ if __name__ == '__main__':
         ratings = sc.textFile(inputV_filepath).map(
             lambda line: map(int, line.split(','))).cache()
 
+    # measure the starting time
     start_time = time.time()
 
     # row / col sizes are max user / movie index
-    row_number = ratings.max(key=itemgetter(0))[0]
-    col_number = ratings.max(key=itemgetter(1))[1]
+    row_num = ratings.max(key=itemgetter(0))[0]
+    col_num = ratings.max(key=itemgetter(1))[1]
 
     # build W and H
-    user_factor = rand(row_number, num_factors)
-    movie_factor = rand(col_number, num_factors)
+    u_factor = rand(row_num, num_factors)
+    m_factor = rand(col_num, num_factors)
 
-    # slice ratings matrix to column groups
-    # TODO: what if not exact division?
-    block_column_size = col_number / num_workers
-    block_row_size = row_number / num_workers
+    blk_col_size = (col_num - 1) / num_workers + 1
+    blk_row_size = (row_num - 1) / num_workers + 1
 
     strata = range(num_workers)
 
     def in_strata(rating_entry):
         user, movie, _ = rating_entry
-        column_group = (movie - 1) / block_column_size
-        return (strata[column_group] * block_row_size <= (user - 1) <
-                (strata[column_group] + 1) * block_row_size)
+        col_group = (movie - 1) / blk_col_size
+        return (strata[col_group] * blk_row_size <= (user - 1) <
+                (strata[col_group] + 1) * blk_row_size)
 
-    def update(group, u_f_p, m_f_p):
+    def update(group, row_start, col_start):
         """
+        Update the incoming user/movie factor matrix with given rating entries
+        :param group: column group number and rating entries
+        :param row_start: starting index of this user partition
+        :param col_start: starting index of this movie partition
+        :return: a tuple of user factor matrix and movie factor matrix, using
+                 the column number as key
+        """
+        col_group, rating_entries = group
 
-        :param group:
-        :param u_f_p:
-        :param m_f_p:
-        :return:
-        """
-        column_group, rating_entries = group
+        # partition of user/movie factor matrix
+        u_f_p = u_factor[row_start:row_start + blk_row_size, :]
+        m_f_p = m_factor[col_start:col_start + blk_col_size, :]
 
         for user, movie, rating in rating_entries:
             # transform real indexes to partitioned factor matrix indexes
-            user_index = (user - 1) % block_row_size
-            movie_index = (movie - 1) % block_column_size
+            user_index = (user - 1) % blk_row_size
+            movie_index = (movie - 1) % blk_col_size
 
             pred_rating = np.dot(u_f_p[user_index, :], m_f_p[movie_index, :])
 
             # TODO: add regularization terms
-            u_gradient = -2 * (rating - pred_rating) * m_f_p[movie_index, :] + \
-                        2 * lambda_value / col_number * u_f_p[user_index, :]
+            u_gradient = -2 * (rating - pred_rating) * m_f_p[movie_index, :]# + \
+                        # 2 * lambda_value / col_number * u_f_p[user_index, :]
             u_f_p[user_index, :] -= beta_value * u_gradient
 
-            m_gradient = -2 * (rating - pred_rating) * u_f_p[user_index, :] + \
-                        2 * lambda_value / row_number * m_f_p[movie_index, :]
+            m_gradient = -2 * (rating - pred_rating) * u_f_p[user_index, :]# + \
+                        # 2 * lambda_value / row_number * m_f_p[movie_index, :]
             m_f_p[movie_index, :] -= beta_value * m_gradient
 
-        return column_group, (u_f_p, m_f_p)
+        return col_group, u_f_p, m_f_p
 
     for _ in range(num_iterations):
-        # split factor matrices
-        user_factors = np.split(user_factor, num_workers, axis=0)
-        movie_factors = np.split(movie_factor, num_workers, axis=0)
-
+        # note in map, `preservesPartitioning` is True
         updated = ratings \
             .filter(lambda r: in_strata(r)) \
-            .groupBy(lambda r: (r[1] - 1) / block_column_size, num_workers) \
+            .groupBy(lambda r: (r[1] - 1) / blk_col_size, num_workers) \
             .map(lambda group: update(group,
-                                      user_factors[strata[group[0]]],
-                                      movie_factors[group[0]]), True) \
+                                      strata[group[0]] * blk_row_size,
+                                      group[0] * blk_col_size), True) \
             .collect()
 
         # aggregate the updates
-        for column_group, (updated_u_f, updated_m_f) in updated:
-            update_start = strata[column_group] * block_row_size
-            update_end = (strata[column_group] + 1) * block_row_size
-            user_factor[update_start:update_end, :] = updated_u_f
+        for column_group, updated_u_f, updated_m_f in updated:
+            update_start = strata[column_group] * blk_row_size
+            update_end = (strata[column_group] + 1) * blk_row_size
+            u_factor[update_start:update_end, :] = updated_u_f
 
-            update_start = column_group * block_column_size
-            update_end = (column_group + 1) * block_column_size
-            movie_factor[update_start:update_end, :] = updated_m_f
+            update_start = column_group * blk_col_size
+            update_end = (column_group + 1) * blk_col_size
+            m_factor[update_start:update_end, :] = updated_m_f
 
         # shift the strata
         strata.append(strata.pop(0))
 
     # do simple evaluation
-    # end_time = time.time()
-    # pred_ratings = np.dot(user_factor, movie_factor.T)
-    # rmse, rating_count = 0.0, 0
-    # for u, m, r in ratings.collect():
-    #     rmse += (r - pred_ratings[u - 1, m - 1]) ** 2
-    #     rating_count += 1
-    #
-    # sc.stop()
-    #
-    # rmse = np.sqrt(rmse / rating_count)
-    # print
-    # print 'RMSE: ' + str(rmse)
-    # print 'time usage: %s seconds' % (end_time - start_time)
+    end_time = time.time()
+    pred_ratings = np.dot(u_factor, m_factor.T)
+    rmse, rating_count = 0.0, 0
+    for u, m, r in ratings.collect():
+        rmse += (r - pred_ratings[u - 1, m - 1]) ** 2
+        rating_count += 1
+
+    sc.stop()
+    loss = rmse
+    rmse = np.sqrt(rmse / rating_count)
+    print
+    print
+    print 'RMSE: ' + str(rmse)
+    print 'loss: ' + str(loss)
+    print 'time usage: %s seconds' % (end_time - start_time)
 
     # write parameters
     with open(outputW_filepath, 'wb') as f:
-        for row in user_factor:
+        for row in u_factor:
             f.write(','.join(map(str, row)) + '\n')
 
     with open(outputH_filepath, 'wb') as f:
-        for row in movie_factor.T:
+        for row in m_factor.T:
             f.write(','.join(map(str, row)) + '\n')
